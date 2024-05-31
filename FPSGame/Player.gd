dd2f5ca306a8a3db82063b17f4d998b04078169e
OhijMik <jihok148@gmail.com>
2024-05-30 20:56:45 -0400

    Added the health bar
    - Added the health bar UI
    - Connected the health bar to the server so that all players can see it

--- FPSGame/Player.gd
+++ FPSGame/Player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal health_changed(health_value)
+
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
 @onready var muzzle_flash = $Camera3D/Pistol/MuzzleFlash
@@ -88,6 +90,7 @@ func receive_damage():
 	if health <= 0:
 		health = 3
 		position = Vector3.ZERO
+	health_changed.emit(health)
 
 
 func _on_animation_player_animation_finished(anim_name):
